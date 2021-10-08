7dec594d7be0b731196fb1a00e59f1d36bd93cbc
Joseph Yekini <joseph@Josephs-MacBook-Pro.local>
2021-10-08 23:30:05 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import firebaseAuth from "./FirebaseAuth";
+import LoginForm from "./Components/LoginForm";
 import "./App.scss";
 
-import firebase from "./FirebaseConfig";
-
 function App() {
+  const [user, setUser] = useState(null);
+
+  firebaseAuth.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
-      <div className="title-row"></div>
-      <h1 className="title">Firebase Recipies</h1>
+      <div className="title-row">
+        <h1 className="title">Firebase Recipies</h1>
+        <LoginForm existingUser={user}></LoginForm>
+      </div>
     </div>
   );
 }
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import firebaseAuth from "../FirebaseAuth";
+
+function LoginForm({ existingUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await firebaseAuth.registerUser(username, password);
+      setUsername(" ");
+      setPassword(" ");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  function handleLogout() {
+    firebaseAuth.logoutUser();
+  }
+
+  return (
+    <div className="login-form-container">
+      {existingUser ? (
+        <div className="row">
+          <h2>Welcome,{existingUser.email}</h2>
+          <button
+            type="button"
+            className="primary-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username(email):
+            <input
+              type="email"
+              required
+              value="username"
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <label className="input-label login-label">
+            Password(password):
+            <input
+              type="password"
+              required
+              value="password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <div className="button-box">
+            <button className="primary-button">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default LoginForm;
